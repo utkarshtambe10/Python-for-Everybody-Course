379239b196e18cde2c9ef9eaa70edf60bfde8395
Utkarsh Tambe <89902664+utkarshtambe10@users.noreply.github.com>
2022-01-18 16:03:31 +0530

    Create assignment5.2.py

---
+++ Course1: Programming for Everybody (Getting Started with Python)/week7/assignment5.2.py
@@ -0,0 +1,20 @@
+largest = -1
+smallest = None
+while True :
+    num = input("Enter a number: ")
+    if num == "done" :
+        break
+    try :
+        numb = int(num)
+    except :
+        print("Invalid input")
+        continue
+    if smallest is None :
+        smallest = numb
+    elif numb < smallest :
+        smallest = numb
+    elif numb > largest :
+        largest = numb
+        
+print("Maximum is", largest)
+print("Minimum is",smallest)
